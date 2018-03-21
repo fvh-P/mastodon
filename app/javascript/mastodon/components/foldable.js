17248a21bc8a1304d642ba12539be88751098102
fvh_P <ko_kurihara@yahoo.co.jp>
2018-03-21 00:46:46 +0000

    Animate folding/opening

--- app/javascript/mastodon/components/foldable.js
+++ app/javascript/mastodon/components/foldable.js
@@ -3,15 +3,28 @@ import PropTypes from 'prop-types';
 import Motion from '../features/ui/util/optional_motion';
 import spring from 'react-motion/lib/spring';
 
-const Foldable = ({ fullHeight, minHeight, isVisible, children }) => (
-  <Motion defaultStyle={{ height: isVisible ? fullHeight : minHeight }} style={{ height: spring(!isVisible ? minHeight : fullHeight) }}>
-    {({ height }) =>
-      (<div style={{ height: `${height}px`, overflow: 'hidden' }}>
-        {children}
-      </div>)
+const Foldable = ({ fullHeight, minHeight, isVisible, children }) => {
+  const defaultHeight = () => {
+    if (isVisible && fullHeight) {
+      return fullHeight;
     }
-  </Motion>
-);
+    else if (isVisible) {
+      return 'auto';
+    }
+
+    return minHeight;
+  };
+
+  return (
+    <Motion defaultStyle={{ height: defaultHeight() }} style={{ height: spring(defaultHeight()) }}>
+      {({ height }) =>
+        (<div style={{ height: `${isVisible && !fullHeight ? 'auto' : `${height}px`}`, overflow: 'hidden' }}>
+          {children}
+        </div>)
+      }
+    </Motion>
+  );
+};
 
 Foldable.propTypes = {
   fullHeight: PropTypes.number.isRequired,
--- app/javascript/mastodon/features/compose/components/announcements.js
+++ app/javascript/mastodon/features/compose/components/announcements.js
@@ -4,6 +4,7 @@ import ImmutablePropTypes from 'react-immutable-proptypes';
 import { injectIntl, defineMessages, FormattedMessage } from 'react-intl';
 import Link from 'react-router-dom/Link';
 import FoldButton from '../../../components/fold_button';
+import Foldable from '../../../components/foldable';
 
 const messages = defineMessages({
   toggle_visible: { id: 'media_gallery.toggle_visible', defaultMessage: 'Toggle visibility' },
@@ -19,8 +20,44 @@ export default class Announcements extends React.PureComponent {
     announcements: ImmutablePropTypes.list.isRequired,
   };
 
+  state = {
+    bodyHeight: null,
+  };
+
+  componentDidMount () {
+    const doc = document.getElementsByClassName('announcements');
+    this.setState({
+      bodyHeight: doc ? doc[0].clientHeight - 34 : null,
+    });
+  }
+
   render () {
     const { intl, visible, onToggle, announcements } = this.props;
+
+    const body = () => {
+      return (
+        announcements.map((announcement, idx) => (
+          <li key={idx}>
+            <div className='announcements__body'>
+              <p dangerouslySetInnerHTML={{ __html: announcement.get('body') }} />
+              <div className='links'>
+                {announcement.get('links').map((link, i) => {
+                  if (link.get('url').indexOf('/') === 0) {
+                    return (
+                      <Link to={link.get('url')} key={i}>{link.get('text')}</Link>
+                    );
+                  } else {
+                    return (
+                      <a href={link.get('url')} target='_blank' key={i}>{link.get('text')}</a>
+                    );
+                  }
+                })}
+              </div>
+            </div>
+          </li>
+        ))
+      );
+    };
 
     return (
       <div className='compose__extra'>
@@ -31,30 +68,11 @@ export default class Announcements extends React.PureComponent {
             <FoldButton title={intl.formatMessage(messages.toggle_visible)} icon='caret-up' onClick={onToggle} size={20} animate active={visible} />
           </div>
         </div>
-        { visible && (
+        <Foldable isVisible={visible} fullHeight={this.state.bodyHeight} minHeight={0} >
           <ul className='announcements'>
-            {announcements.map((announcement, idx) => (
-              <li key={idx}>
-                <div className='announcements__body'>
-                  <p dangerouslySetInnerHTML={{ __html: announcement.get('body') }} />
-                  <div className='links'>
-                    {announcement.get('links').map((link, i) => {
-                      if (link.get('url').indexOf('/') === 0) {
-                        return (
-                          <Link to={link.get('url')} key={i}>{link.get('text')}</Link>
-                        );
-                      } else {
-                        return (
-                          <a href={link.get('url')} target='_blank' key={i}>{link.get('text')}</a>
-                        );
-                      }
-                    })}
-                  </div>
-                </div>
-              </li>
-            ))}
+            {body}
           </ul>
-        )}
+        </Foldable>
       </div>
     );
   }
